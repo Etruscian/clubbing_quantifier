d4183b725e403a54ab9fbd07d01af85e7c6675a9
Etruscian <sanswart@gmail.com>
2017-05-16 17:12:28 +0200

    Added database logging and fixed issues with starting a new assessment after one has been completed

--- FingerGUI.py
+++ FingerGUI.py
@@ -1,13 +1,14 @@
 import os
 import threading
-import time
 import tkinter as tk
+import io
 
 import cv2
 import imutils
 import numpy as np
 from PIL import Image, ImageTk
 from imutils.video import VideoStream
+from DatabaseConnection import DatabaseHandler
 
 # from Old.fingerscanner_working import calculate
 
@@ -30,16 +31,24 @@ btnpad = 5
 
 # Declaration of GUI class
 class VingerGUI:
+
+    databasehandler = None
+
     def __init__(self):
         global root
+        self.databasehandler = DatabaseHandler('database/testDB.db')
+        data = {'event': 'program start'}
+        self.databasehandler.adddata('events', **data)
         # Call the start page
-        Startpage(self)
+        Startpage(self, self.databasehandler)
 
     def show_frame(self, cont):
         cont(self)
 
 
 class Startpage:
+
+    databasehandler = None
 
     frame = None
     resultframe = None
@@ -50,6 +59,7 @@ class Startpage:
 
     image = None
     imagelabel = None
+    rawimage = None
 
     fingertipmarker = None
     nailbedmarker = None
@@ -73,20 +83,23 @@ class Startpage:
     stopEvent = None
 
     # Initializes the startpage.
-    def __init__(self, controller):
+    def __init__(self, controller, databasehandler):
         global root
 
         # Instantiate the controller handling the startpage
-        Startpage.controller = controller
+        self.controller = controller
+
+        # Get databasehandler and store it inside class
+        self.databasehandler = databasehandler
 
         # Create the background frame, make it full screen and set its color to white
-        Startpage.frame = tk.Frame(root, bg="white")
-        Startpage.frame.pack()
-        Startpage.frame.place(relheight=1.0, relwidth=1.0)
+        self.frame = tk.Frame(root, bg="white")
+        self.frame.pack()
+        self.frame.place(relheight=1.0, relwidth=1.0)
 
         # Create the buttonframe, which stores all buttons. Its color is set to black for testing purposes
         self.btnFrame = tk.Frame(root, bg="black")
-        self.btnFrame.pack(in_=Startpage.frame)
+        self.btnFrame.pack(in_=self.frame)
         self.btnFrame.place(height=screenheight, width=btnwidth)
 
         # Create the Preview camera button and place it in the buttonframe
@@ -127,7 +140,7 @@ class Startpage:
 
         # Create the resultframe inside the backgroundframe and set its color to green for testing purposes
         self.resultframe = tk.Frame(root, bg="green")
-        self.resultframe.pack(in_=Startpage.frame)
+        self.resultframe.pack(in_=self.frame)
         self.resultframe.place(x=btnwidth, height=screenheight, width=screenwidth - btnwidth)
 
         self.ratioframe = tk.Frame(self.resultframe, bg="red")
@@ -141,7 +154,7 @@ class Startpage:
         self.anglelabel = tk.Label(self.angleframe, text="Angle")
 
         self.previewFrame = tk.Frame(root, bg="green")
-        self.previewFrame.pack(in_=Startpage.frame)
+        self.previewFrame.pack(in_=self.frame)
         self.previewFrame.place(x=btnwidth, height=screenheight, width=screenwidth - btnwidth)
 
         self.stopEvent = threading.Event()
@@ -157,7 +170,6 @@ class Startpage:
             self.nailbedmarker = self.imagelabel.create_oval((event.x, event.y, event.x, event.y), width=3.0)
         elif self.nailbedline is None:
             self.nailbedpoint += event.x, event.y
-            print(self.nailbedpoint)
             self.nailbedline = self.imagelabel.create_line(self.nailbedpoint[0], self.nailbedpoint[1],
                                                            self.nailbedpoint[2], self.nailbedpoint[3], width=3.0)
         elif not self.jointpoint:
@@ -202,12 +214,21 @@ class Startpage:
     def returntostart(self):
         self.resultframe.pack_forget()
         self.resultframe.place_forget()
-        self.previewFrame.pack(in_=Startpage.frame)
+        self.previewFrame.pack(in_=self.frame)
         self.previewFrame.place(x=btnwidth, height=screenheight, width=screenwidth - btnwidth)
         self.stopEvent = threading.Event()
         self.thread = threading.Thread(target=self.videoLoop)
         self.thread.start()
-        self.photobutton.config(command=lambda: Startpage.takePicture(self))
+        icon = Image.open("./icons/camera.png").resize((int(btnwidth), int(btnwidth)), Image.ANTIALIAS)
+        icon = ImageTk.PhotoImage(icon)
+        self.photobutton.config(command=lambda: Startpage.takePicture(self), image=icon)
+        self.photobutton.image = icon
+        self.fingertippoint = None
+        self.fingertipmarker = None
+        self.nailbedpoint = None
+        self.nailbedline = None
+        self.jointpoint = None
+        self.jointline = None
 
     def takePicture(self):
         if self.stopEvent is not None:
@@ -231,6 +252,9 @@ class Startpage:
 
     def videoLoop(self):
 
+        data = {'event': 'start preview camera'}
+        self.databasehandler.adddata('events', **data)
+
         panel = None
 
         vs = VideoStream(usePiCamera=False).start()
@@ -240,8 +264,8 @@ class Startpage:
                 if currentFrame is not None:
                     currentFrame = imutils.resize(currentFrame, width=int(screenwidth - btnwidth))
                     image = cv2.cvtColor(currentFrame, cv2.COLOR_BGR2RGB)
-                    image = Image.fromarray(image)
-                    self.image = ImageTk.PhotoImage(image)
+                    self.rawimage = Image.fromarray(image)
+                    self.image = ImageTk.PhotoImage(self.rawimage)
 
                     if panel is None:
                         panel = tk.Label(self.previewFrame, image=self.image)
@@ -252,15 +276,20 @@ class Startpage:
                         panel.configure(image=self.image)
                         panel.image = self.image
 
-        except RuntimeError:
+        except RuntimeError as e:
             print("RuntimeError")
+            data = {'event': e}
+            self.databasehandler.adddata('events', **data)
 
         panel.place_forget()
         panel.pack_forget()
 
         vs.stop()
+        data = {'event': 'stop preview camera'}
+        self.databasehandler.adddata('events', **data)
 
     def closeProgram(self):
+
         top = tk.Toplevel()
         top.resizable(0, 0)
         top.title('Shutdown')
@@ -276,6 +305,10 @@ class Startpage:
         cancelBtn.place(relheight=0.5, width=66, x=209, y=25)
 
     def calculateRatio(self):
+
+        data = {'event': 'calculate'}
+        self.databasehandler.adddata('events', **data)
+
         maxjointvalue = max((self.jointpoint[3], self.jointpoint[1]))
         maxjointindex = self.jointpoint.index(maxjointvalue)
         maxnailbedvalue = max((self.nailbedpoint[3], self.nailbedpoint[1]))
@@ -305,10 +338,28 @@ class Startpage:
         self.anglelabel["text"] = angle
         self.ratiolabel["text"] = ratio
 
+        data['event'] = 'calculation complete'
+
+        self.databasehandler.adddata('events', **data)
+
+        output = io.BytesIO()
+        self.rawimage.save(output, format='JPEG')
+
+        data['image'] = output.getvalue()
+        data['ratio'] = ratio
+        data['angle'] = angle
+        data['fingertip'] = str(self.fingertippoint)
+        data['nailbed'] = str(self.nailbedpoint)
+        data['joint'] = str(self.jointpoint)
+
+        self.databasehandler.adddata('image_data', **data)
+
         self.previewFrame.pack_forget()
         self.previewFrame.place_forget()
         self.imagelabel.destroy()
 
+        self.resultframe.pack(in_=self.frame)
+        self.resultframe.place(x=btnwidth, height=screenheight, width=screenwidth - btnwidth)
         self.ratioframe.pack(in_=self.resultframe)
         self.ratioframe.place(height=0.5*screenheight, width=screenwidth-btnwidth)
         self.angleframe.pack(in_=self.resultframe)
